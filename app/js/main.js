33b58dd20f9e62bf3f9987f5980920c70dcbaffa
Bulgakov Konstantin <nor.wanderer@gmail.com>
2016-05-24 12:30:57 +0300

    now tabs are working on javascript

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,30 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     var modal = $('.modal');
 
-    $('.header_button-log').click(function(){
+    $('.header_button-log').click(function() {
         modal.fadeIn(600);
         $('.login').fadeIn(600);
         $('body').toggleClass('hidden_mod');
     });
-    $('.intro_info-button').click(function(){
+    $('.intro_info-button').click(function() {
         modal.fadeIn(600);
         $('.registration').fadeIn(600);
         $('body').toggleClass('hidden_mod');
     });
-    $('#forgot').click(function(){
+    $('#forgot').click(function() {
         $('.login').fadeOut(400);
         $('.change-password').delay(400).fadeIn(400);
         $('body').toggleClass('hidden_mod');
     });
-    
 
 
 
-    modal.click(function(e){
-        var target = $(e.target)
-        if (target.hasClass('modal')){
+
+    modal.click(function(e) {
+        event = e || window.event;
+        var target = $(event.target) || $(event.srcElement);
+        if (target.hasClass('modal')) {
             modal.fadeOut(600);
             $('.modal>div').fadeOut(600);
             $('body').removeClass('hidden_mod');
--- app/js/property.js
+++ app/js/property.js
@@ -1,72 +1,85 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $('.header_setting-but').click(function(){
-        
+    $('.header_setting-but').click(function() {
+
         $('.header_setting-menu').slideToggle(200);
         $('.header_setting-but').toggleClass('active');
     })
 
     var filter = $('.modal')
 
-    $('.expenses .edit').click(function(){
-    filter.fadeIn(600);
-    $('body').toggleClass('hidden_mod');
+    $('.expenses .edit').click(function() {
+        filter.fadeIn(600);
+        $('body').toggleClass('hidden_mod');
     })
 
-    filter.click(function(e){
-        var target = $(e.target);
-        if (target.hasClass('modal')){
+    filter.click(function(e) {
+        event = e || window.event;
+        var target = $(event.target) || $(event.srcElement);
+        if (target.hasClass('modal')) {
             filter.fadeOut(600);
             $('body').toggleClass('hidden_mod');
         };
     });
 
-    $('.editing_paid input').click(function(){
+    $('.tabs_wr').click(function(e){
+    event = e || window.event;
+    var target = $(event.target) || $(event.srcElement);
+    
+    var dataName = target.data('tab');
+    $(this).children().removeClass("mod-active");
+    target.addClass('mod-active');
+    $('.tabs_box-wrap').children().removeClass('mod-box-active');
+    $('[data-for-tab =' + dataName + ']').addClass('mod-box-active');
+    
+    })
+
+    $('.editing_paid input').click(function() {
         $('.editing_paid label').removeClass('editing_paid-check')
         $(this).parent().toggleClass('editing_paid-check')
     });
 
 
     /*Bihavior for upload input*/
-     var wrapper = $( ".editing_file-upload" ),
-        inp = wrapper.find( "input" ),
-        btn = wrapper.find( ".editing_file-button" ),
-        lbl = wrapper.find( ".editing_file-mark" );
+    var wrapper = $(".editing_file-upload"),
+        inp = wrapper.find("input"),
+        btn = wrapper.find(".editing_file-button"),
+        lbl = wrapper.find(".editing_file-mark");
 
-    var file_api = ( window.File && window.FileReader && window.FileList && window.Blob ) ? true : false;
+    var file_api = (window.File && window.FileReader && window.FileList && window.Blob) ? true : false;
 
-        inp.change(function(){
+    inp.change(function() {
         var file_name;
-        if( file_api && inp[ 0 ].files[ 0 ] )
-            file_name = inp[ 0 ].files[ 0 ].name;
+        if (file_api && inp[0].files[0])
+            file_name = inp[0].files[0].name;
         else
-            file_name = inp.val().replace( "C:\\fakepath\\", '' );
+            file_name = inp.val().replace("C:\\fakepath\\", '');
 
-        if( ! file_name.length )
+        if (!file_name.length)
             return;
 
-        if( lbl.is( ":visible" ) ){
-            lbl.text( file_name );
-            btn.text( "Select..." );
-        }else
-            btn.text( file_name );
+        if (lbl.is(":visible")) {
+            lbl.text(file_name);
+            btn.text("Select...");
+        } else
+            btn.text(file_name);
     }).change();
 
-    $( window ).resize(function(){
-    $( ".editing_file-upload" ).triggerHandler( "change" );
+    $(window).resize(function() {
+        $(".editing_file-upload").triggerHandler("change");
     });
 
     /* Settings for chosen plugin*/
 
     $('#upply').chosen({
-        width:"100%",
+        width: "100%",
         disable_search_threshold: 10
     });
     $('#frequency').chosen({
-        width:"110px",
+        width: "110px",
         disable_search_threshold: 10
     });
-    
+
 
 
 
